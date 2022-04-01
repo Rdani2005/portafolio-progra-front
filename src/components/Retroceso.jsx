86748bfe790d08ef6320dad25a69c99f78c84f72
Rdani2005 <dricardosc15@gmail.com>
2022-04-01 08:33:19 -0600

    Listos los botones, diseño 100% responsivo

--- src/components/Retroceso.jsx
+++ src/components/Retroceso.jsx
@@ -6,12 +6,11 @@ const Retroceso = ({ link }) => {
     return (
         <section className="container mt-5">
 
-            <div className="d-flex justify-content-center align-items-center">
-                <button onClick={() => navigate(-1)} className="button mx-4">Anterior</button>
-                <Link to="/" className="button mx-4">Home</Link>
-                <a href={link} className="button mx-4">Descargar Archivos</a>
+            <div className="d-flex flex-wrap justify-content-center align-items-center">
+                <button onClick={() => navigate(-1)} className="button mx-4 my-4">Anterior</button>
+                <Link to="/" className="button mx-4 my-4">Home</Link>
+                <a href={link} className="button mx-4 my-4">Descargar Archivos</a>
             </div>
-
         </section>
     )
 }
