824c9634654e16198e97eac775728f967cd039b6
rdani2005 <dricardosc15@gmail.com>
2022-06-11 18:57:29 -0600

    Changed some titles on Cotejos view

--- src/views/Evaluaciones/Cotejos.jsx
+++ src/views/Evaluaciones/Cotejos.jsx
@@ -23,7 +23,7 @@ const Cotejos = () => {
     <main className="d-flex justify-content-center align-items-center flex-column p-5 bg-dark text-center min-vh-100">
       <h1 className="display-1 mt-5">Hojas de Cotejos</h1>
       <section className="mt-5">
-        <h3>Aqui se enseña la informacion importante sobre los examenes!</h3>
+        <h3>Aqui veremos las hojas de cotejo</h3>
       </section>
       <section className="container mt-5">
         <div className="row">
